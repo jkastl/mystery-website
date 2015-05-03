56262cd51d8788a9920f7a5b5162eb507d47b19c
Sara Heins <saraeheins@gmail.com>
2015-05-03 06:30:18 -0500

    Small style changes

--- wordpress/wp-content/themes/zerif-lite/content-events.php
+++ wordpress/wp-content/themes/zerif-lite/content-events.php
@@ -15,7 +15,7 @@
 	$the_query = new WP_Query($args);?>
 		<?php if (have_posts() ): while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-		<section id="<?php post_class(); ?>" class="event">
+		<section id="<?php post_class(); ?>">
 			<h6><?php the_title(); ?></h6>
 			<h6><?php echo get_field('month'); ?></h6>
 			<p><?php the_excerpt(); ?></p>
