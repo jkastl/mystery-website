7ff5c5c6c1273628aea2268ee99a1de423410c89
Sara Heins <saraeheins@gmail.com>
2015-05-10 21:26:04 -0500

    Add favicon, fix some events page stuff

--- wordpress/wp-content/themes/zerif-lite/header.php
+++ wordpress/wp-content/themes/zerif-lite/header.php
@@ -23,6 +23,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" />
 
 <!--[if lt IE 9]>
 <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
